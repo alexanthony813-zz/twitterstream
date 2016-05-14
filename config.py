fa1917a17c6f20a9a5090f53a0f76106fa0f7673
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-14 13:36:14 -0700

    Adding variables to heroku

---
+++ config.py
@@ -0,0 +1,13 @@
+api_ckey = 'jEvzsLn3hcZogmtOVvfP0Gx0A'
+api_csecret = 'uWClVOYHuBIoV7nOdItRIeFgRUtrms9jk0OesT9sOWWk3WOd6W'
+api_atoken = '730880914692046848-ZtqJuUTqhbcPfbeW1iKWB8DqTRWxSlr'
+api_asecret = 'Fct3KwbH7HDdQah1byXfeP3yDWk7eaLStnuAIJxY9Ej8M'
+
+MONGO_DEV_URL = 'localhost'
+MONGO_DEV_PORT = 27017
+
+REDIS_DEV_URL = 'localhost'
+REDIS_DEV_PORT = 6379
+REDIS_PROD_URL = 'redis://h:p8i3rsblvdebehclclbqc29sdfc@ec2-54-227-250-222.compute-1.amazonaws.com:9449'
+REDIS_PROD_PORT = 9449
+REDIS_PROD_PASSWORD = 'p8i3rsblvdebehclclbqc29sdfc'
