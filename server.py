b4600ba09d7a2094fb1139a901b0902afc5a2fee
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-15 03:29:13 -0700

    making sure connection is defined

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ def connect():
             os.wait()
         else:
             os.fork()
-            connection = MongoClient(uri, port=23442, maxPoolSize=50, waitQueueMultiple=10, connect=False)
+        connection = MongoClient(uri, port=23442, maxPoolSize=50, waitQueueMultiple=10, connect=False)
     else:
         connection = MongoClient('localhost', MONGO_DEV_PORT, maxPoolSize=50, waitQueueMultiple=10, connect=False)
     handle = connection['heroku_0p1s62cb']
