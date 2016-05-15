e734258a6710642de118350de40496fe593b3258
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-14 22:59:47 -0700

    Changed port number

--- server.py
+++ server.py
@@ -82,7 +82,7 @@ def get_sentiment(lat, lon, km_radius):
 # Remove the "debug=True" for production
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 23442))
+    port = int(os.environ.get('PORT', 5000))
     if is_prod:
         app.run(host='0.0.0.0', port=port)
     else:
