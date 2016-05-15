7d637e89b05df1270dcbfbc0566d5daa3f50bff1
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-14 21:04:44 -0700

    Fixing correct port in app.run

--- server.py
+++ server.py
@@ -85,6 +85,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 27017))
     if is_prod:
         print 'we\'re doin it live!\n\n\n\n\!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!n\n\n\n\n'
-        app.run(host='0.0.0.0', port=port)
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 23442))
     else:
         app.run(host='localhost', port=port, debug=True, threaded=True)
