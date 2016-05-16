704cee9f43d4205eb7b9856222d8698003a0737e
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-16 02:27:40 -0700

    Took mongo backend out of celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,12 +1,12 @@
 from celery.schedules import crontab
 
-CELERY_RESULT_BACKEND = "mongodb"
-CELERY_MONGODB_BACKEND_SETTINGS = {
-    "host": "127.0.0.1",
-    "port": 27017,
-    "database": "'heroku_0p1s62cb'",
-    "taskmeta_collection": "stock_taskmeta_collection",
-}
+# CELERY_RESULT_BACKEND = "mongodb"
+# CELERY_MONGODB_BACKEND_SETTINGS = {
+#     "host": "127.0.0.1",
+#     "port": 27017,
+#     "database": "'heroku_0p1s62cb'",
+#     "taskmeta_collection": "stock_taskmeta_collection",
+# }
 
 #used to schedule tasks periodically and passing optional arguments
 #Can be very useful. Celery does not seem to support scheduled task but only periodic
